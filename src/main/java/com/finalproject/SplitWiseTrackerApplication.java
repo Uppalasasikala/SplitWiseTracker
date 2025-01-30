95c45bd60bc717ea7903e964ddce11b9fc9a3a97
Uppalasasikala <sasihoney2102001@gmail.com>
2025-01-30 17:21:32 +0530

    Initial commit

---
+++ src/main/java/com/finalproject/SplitWiseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitWiseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitWiseTrackerApplication.class, args);
+	}
+
+}
